47107044b1ea457a8adf7b5b26af04ca1fe84266
Andrey Helldar <helldar@ai-rus.com>
2021-05-25 23:26:56 +0300

    Namespace refactoring

--- src/Driver.php
+++ src/Driver.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace Helldar\CashierSber;
+namespace Helldar\CashierDriver\Sber;
 
 use Helldar\Cashier\Services\Driver as BaseDriver;
 
 class Driver extends BaseDriver
 {
+    public const NAME = 'sber';
 }
