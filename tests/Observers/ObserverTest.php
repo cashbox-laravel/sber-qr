7309bd87996c6b413cbe15129c595e7efe22ab1b
Andrey Helldar <andrey-helldar@users.noreply.github.com>
2021-08-25 23:40:54 +0000

    Apply fixes from StyleCI

--- tests/Observers/ObserverTest.php
+++ tests/Observers/ObserverTest.php
@@ -32,14 +32,6 @@ class ObserverTest extends TestCase
 
     protected $model = RequestPayment::class;
 
-    protected function getPackageProviders($app): array
-    {
-        return [
-            ServiceProvider::class,
-            ObserverServiceProvider::class,
-        ];
-    }
-
     public function testCreate()
     {
         $this->assertSame(0, DB::table('payments')->count());
@@ -105,6 +97,14 @@ class ObserverTest extends TestCase
             PaymentConfig::getStatuses()->getStatus(Status::SUCCESS),
             $payment->status_id
         );
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ServiceProvider::class,
+            ObserverServiceProvider::class,
+        ];
     }
 
     protected function payment(): RequestPayment
