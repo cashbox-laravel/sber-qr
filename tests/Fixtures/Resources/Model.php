bb88ff357365a4fab360f4993de16cc625a14474
Andrey Helldar <helldar@dragon-code.pro>
2023-07-11 02:18:20 +0300

    Update copyrights

--- tests/Fixtures/Resources/Model.php
+++ tests/Fixtures/Resources/Model.php
@@ -1,18 +1,16 @@
 <?php
 
-/*
- * This file is part of the "cashier-provider/sber-qr" project.
+/**
+ * This file is part of the "cashier-provider/foundation" project.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author Andrey Helldar <helldar@ai-rus.com>
- *
- * @copyright 2021 Andrey Helldar
- *
+ * @author Andrey Helldar <helldar@dragon-code.pro>
+ * @copyright 2023 Andrey Helldar
  * @license MIT
  *
- * @see https://github.com/cashier-provider/sber-qr
+ * @see https://github.com/cashier-provider/foundation
  */
 
 namespace Tests\Fixtures\Resources;
